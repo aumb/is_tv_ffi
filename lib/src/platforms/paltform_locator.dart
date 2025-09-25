d4421f6e9b401889ef576491ff4fe285dcaae234
Mathiew Abbas <mathiew95@gmail.com>
2025-09-25 11:47:27 -0500

    fix: typo in platform locator import

--- lib/src/platforms/paltform_locator.dart
+++ lib/src/platforms/paltform_locator.dart
@@ -1,6 +1,6 @@
 import 'package:is_tv_ffi/src/is_tv.dart';
 import 'package:is_tv_ffi/src/platforms/stub.dart' // Default fallback
-    if (dart.library.io) 'package:is_tv_ffi/src/platforms/platorms.dart';
+    if (dart.library.io) 'package:is_tv_ffi/src/platforms/platforms.dart';
 
 /// Initializes the correct instance for the platform
 IsTv get platformInstance => getIsTvInstance();
